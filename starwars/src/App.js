a91e968c2fc0293e4175c5f8458843163e7349be
alfredo villasenor <alfredo.villasenor.aof@gmail.com>
2020-03-20 10:13:13 -0700

    finished apps.js i think i forgot to commit

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import cards from './components/Cards'
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+    const [data, setData] = useState([]);
 
+    useEffect(() => {
+      axios
+      .get('https://swapi.co/api/people/')
+      .then(response => {
+        console.log(response.data)
+        setData(response.data.result)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }, [])
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
